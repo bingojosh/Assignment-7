ccdbe65c21d0309500072f68674fb05d81ab706a
Josh LaRochelle <joshua.r.larochelle@gmail.com>
2019-01-10 19:26:27 -0500

    Close enuf

--- main.js
+++ main.js
@@ -40,21 +40,19 @@ $(document).ready(function(){
         console.log("There was an error " + errorObject)
     })
 
-    if()
-        setInterval(() => {
-            $("#tbody").html("");
-            database.ref("/trainData").on("value",(snapshot) => {
-                database.ref("/trainData").on("child_added", function(snapshot){
-                    var name = snapshot.val().name
-                    var destination = snapshot.val().destination
-                    var departure = snapshot.val().departure
-                    var frequency = snapshot.val().frequency
-                    tablePrint(name, destination, departure, frequency)
-                }, function(errorObject){
-                    console.log("There was an error " + errorObject)
-                })
-            })
-        }, 60000)
+
+    setInterval(() => {
+        $("#tbody").html("");
+        database.ref("/trainData").on("child_added", function(snapshot){
+            var name = snapshot.val().name
+            var destination = snapshot.val().destination
+            var departure = snapshot.val().departure
+            var frequency = snapshot.val().frequency
+            tablePrint(name, destination, departure, frequency)
+        }, function(errorObject){
+            console.log("There was an error " + errorObject)
+        })
+    }, 60)
 
 })
 
